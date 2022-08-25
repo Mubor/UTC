8f05ce2a2b16c51b24107bbd14c843c1ac269b4b
Mubor <muhatasov22@ukr.net>
2022-08-25 15:02:42 +0300

    body overflow menu fix

--- src/menu.js
+++ src/menu.js
@@ -4,8 +4,8 @@ const header = document.querySelector('header');
 const menu = document.querySelector('.header__menu');
 
 const switchMenu = () => {
-    // menu.classList.toggle('open');
     header.classList.toggle('open');
+    document.body.classList.toggle('overflow');
 
     if (header.classList[header.classList.length-1] === 'open') {
         menuButton.innerHTML = 'close menu';

10189d30b476fdc51dcdb2ad7a02b1ed26ca50be
Mubor <muhatasov22@ukr.net>
2022-08-26 20:31:08 +0300

    animation fixed

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,10 @@ const menuButton = document.getElementById('menu-button');
 const header = document.querySelector('header');
 const menu = document.querySelector('.header__menu');
 
+const changeContent = (element, content) => {
+    element.innerHTML = content;
+}
+
 const switchMenu = () => {
     header.classList.toggle('open');
     document.body.classList.toggle('overflow');
@@ -18,15 +22,15 @@ menuButton.addEventListener('click', switchMenu);
 
 //type effect
 
+
 const type = new Typed('#type-container', {
-    strings: ['hello dude', 'hello dude '],
+    strings: ['hello dude ', 'hello dude '],
     // stringsElement:null,
     loop:true,
     loopCount: Infinity,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 10000
+    typeSpeed: 200,
+    backSpeed: 60,
+    backDelay: 6000
 });
-debugger;
 type.start();
 

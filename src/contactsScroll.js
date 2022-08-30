89890b2b37ae932501ab2d7ed5caf7449d596f2f
Mubor <muhatasov22@ukr.net>
2022-08-30 23:02:26 +0300

    js refactore, colors fixed

---
+++ src/contactsScroll.js
@@ -0,0 +1,3 @@
+const mediaContainer = document.getElementById('scroll-media');
+const scrollCenter = 989/2 - mediaContainer.clientWidth/2;
+mediaContainer.scrollTo(scrollCenter, 0);
--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,6 @@
 const menuButton = document.getElementById('menu-button');
 const header = document.querySelector('header');
 const menu = document.querySelector('.header__menu');
-const bl = 0;
-
-const changeContent = (element, content) => {
-    element.innerHTML = content;
-}
 
 const switchMenu = () => {
     header.classList.toggle('open');
@@ -21,46 +16,6 @@ const switchMenu = () => {
 
 menuButton.addEventListener('click', switchMenu);
 
-const mediaContainer = document.getElementById('scroll-media');
-const scrollCenter = 989/2 - mediaContainer.clientWidth/2;
-mediaContainer.scrollTo(scrollCenter, 0);
-//type effect
-
-const initTypeAnimation = (typeText, isLooped = true) => {
-    return new Typed('#type-container', {
-        strings: [typeText],
-        loop: isLooped,
-        loopCount: Infinity,
-        typeSpeed: 200,
-        backSpeed: 60,
-        backDelay: 6000,
-    });
-}
-
-const typeText = document.getElementById('type-source').innerHTML;
-const isLooped = JSON.parse(document.getElementById('type-loop').innerHTML);
-let typeAnimation;
-let animationCanStart = true;
-
-if(document.documentElement.clientWidth > 830) {
-    typeAnimation = initTypeAnimation(typeText, isLooped)
-    typeAnimation.start();
-    animationCanStart = false;
-} else {
-    typeAnimation.destroy();
-}
-
-window.addEventListener('resize', () => {
-    if(document.documentElement.clientWidth <= 831 && typeAnimation) {
-        typeAnimation.destroy();
-        animationCanStart = true
-    }
-    else if (animationCanStart) {
-        typeAnimation = initTypeAnimation(typeText, isLooped);
-        typeAnimation.start();
-        animationCanStart = false;
-    }
-});
 
 
 

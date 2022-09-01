6c0a91d6194dc9701743603e480bae0a4547175a
Mubor <muhatasov22@ukr.net>
2022-09-01 17:22:13 +0300

    .env fix

--- src/app/contacts/contacts.js
+++ src/app/contacts/contacts.js
@@ -1,7 +1,22 @@
 import menuActivator from "../../lib/menu";
 
+
+
+const API_KEY=process.env.API_KEY;
+const WEB_CLIENT=JSON.parse(process.env.WEB_CLIENT);
+async function loadSavedCredentialsIfExist() {
+    try {
+    //   const content = await fs.readFile(TOKEN_PATH);
+      const credentials = JSON.parse(WEB_CLIENT);
+      // return google.auth.fromJSON(credentials);
+    } catch (err) {
+      return null;
+    }
+}
+
 const mediaContainer = document.getElementById('scroll-media');
 const scrollCenter = 989/2 - mediaContainer.clientWidth/2;
 mediaContainer.scrollTo(scrollCenter, 0);
 
 menuActivator('menu-button', 'header');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -5,7 +7,22 @@ module.exports = {
         contacts: './src/app/contacts/contacts.js',
         simple: './src/app/simple/simple_page.js',
     },
-    // resolve: {
-    //     extensions: ['.js']
-    // }
+    resolve: {
+        fallback: {
+          "fs": false,
+          "tls": false,
+          "net": false,
+          "path": false,
+          "zlib": false,
+          "http": false,
+          "https": false,
+          "stream": false,
+          "crypto": false,
+          "googleapis": false,
+          "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+        } 
+      },
+      plugins: [
+        new Dotenv()
+    ],
   };

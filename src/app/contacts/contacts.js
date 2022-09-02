a74e1e88d534b45bd045096a07e9defa67576967
Mubor <muhatasov22@ukr.net>
2022-09-02 18:23:00 +0300

    ntl fix CSP 7

--- src/app/contacts/contacts.js
+++ src/app/contacts/contacts.js
@@ -3,8 +3,8 @@ import menuActivator from "../../lib/menu";
 
 const API_KEY="AIzaSyC0HBc92apvJtst7CPtIxTttglASvkMp3M";
 const WEB_CLIENT=JSON.parse('{"web":{"client_id":"659403616723-1i3q9h1d0emeanbbr5cqisk83uegto1a.apps.googleusercontent.com","project_id":"utcfilm","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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"}}').web;
-const SCOPES = ['https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/profile'];
-const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+const SCOPES = ['https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/calendar.events'];
+const CALENDAR_ID ="hu6ioahhamk7m480jtm1sdm4n0@group.calendar.google.com";
 
         
 const mediaContainer = document.getElementById('scroll-media');
